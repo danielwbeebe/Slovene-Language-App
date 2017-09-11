2a5a540d6d5186518d6e3dc62acf853181e42267
Daniel Beebe <danwbeebe@gmail.com>
2017-09-11 15:58:39 -0400

    add migration file

---
+++ db/migrations/migration_09_11_2017.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS questions;
+
+CREATE TABLE users(
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR UNIQUE NOT NULL,
+    email VARCHAR UNIQUE NOT NULL,
+    password_digest TEXT NOT NULL,
+    level INT
+);
+
+CREATE TABLE questions(
+    id BIGSERIAL PRIMARY KEY,
+    level INT NOT NULL,
+    question TEXT,
+    right_answer TEXT,
+    wrong_answer_1 TEXT,
+    wrong_answer_2 TEXT,
+    correct BOOLEAN NOT NULL,
+    user_id INT REFERENCES users(id) NOT NULL
+);
