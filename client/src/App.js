229d32231db26f6d9820650e59cbb02dfaeb28a6
Daniel Beebe <danwbeebe@gmail.com>
2017-09-11 16:47:50 -0400

    set up welcome page

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,93 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { Redirect } from 'react-router';
+
+import Welcome from './components/Welcome';
+
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      redirect: false,
+      redirecting: '',
+    }
+  }
+
+  handleLoginSubmit = (e, username, password) => {
+    e.preventDefault();
+    axios.post('/auth/login', {
+      username,
+      password,
+    }).then(res => {
+      console.log(res.data.user)
+
+      this.setState({
+        auth: res.data.auth,
+        user: res.data.user,
+      });
+      this.handleRedirect('/main')
+    }).catch(err => console.log(err));
+  }
+
+  handleRegisterSubmit = (e, options) => {
+    e.preventDefault();
+    axios.post('/auth/register', {
+      options
+    })
+    .then(res => {
+      console.log("Options:")
+      console.log(options)
+      console.log('Res Data:')
+      console.log(res.data)
+      this.setState({
+        auth: res.data.auth,
+        user: res.data.user,
+      })
+      window.location.href = '/'
+    })
+    .catch(err => console.log(err));
+  }
+
+  handleRedirect = (path) => {
+    this.setState({
+      redirect: true,
+      redirecting: path,
+    })
+    console.log(`The path is: ` + path)
+
+  }
+
+  logOut() {
+    axios.get('/auth/logout')
+      .then(res => {
+        this.setState({
+          auth: false,
+          currentPage: 'welcome',
+        })
+      }).catch(err => console.log(err));
+  }
+
+  redirectTo = () => {
+    if(this.state.redirect){
+      console.log('redirect')
+      this.setState({
+        redirect: false,
+      })
+      return(<Redirect to={this.state.redirecting}/>)
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div className="App">
+          <Route exact path="/" render={() => <Welcome />} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Welcome = () => {
+
+  return (
+    <div className="welcome">
+
+      <div className="header">
+       <h1>slavolingo</h1>
+      </div>
+
+      <div className="main">
+        <h1>This is the Welcome Page</h1>
+      </div>
+
+      <div className="footer">
+       <h1>Created By Dan</h1>
+      </div>
+
+    </div>
+  )
+}
+
+export default Welcome;
