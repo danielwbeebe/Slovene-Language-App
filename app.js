507ad9a9d9b10bcf3148bc99cad98a89ae1b0701
Daniel Beebe <danwbeebe@gmail.com>
2017-09-11 16:01:48 -0400

    add code to app.js file

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, ()=>{
+    console.log(`listening on port ${PORT}!`);
+});
+
+app.use(logger('dev'));
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.use(cookieParser());
+app.use(session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const authRoutes = require('./routes/auth-routes.js');
+app.use('/auth', authRoutes);
+
+const userRoutes = require('./routes/user-routes');
+app.use('/user', userRoutes);
+
+const questionRoutes = require('./routes/question-routes.js');
+app.use('/questions', questionRoutes);
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+app.get('*', (req, res) => {
+  res.status(404).json({
+    message: 'Invalid route!',
+  });
+});
