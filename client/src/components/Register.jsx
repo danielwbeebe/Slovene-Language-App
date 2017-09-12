adf75f64ba059250614d0cfe075c6d3370e149fb
Daniel Beebe <danwbeebe@gmail.com>
2017-09-12 16:47:01 -0400

    add css styles to register page

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,29 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 
-const Register = () => {
+class Register extends Component{
 
-  return (
-    <div className="register">
+// method to handle addition of new user
 
-      <div className="register-header">
+  handleInputChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
 
-        <div className="header-message">
-          <h1>slovolingo</h1>
+// method to render page with form for registration
+  render(){
+    return (
+      <div className="register">
+
+        <div className="register-header">
+
+          <div className="header-message">
+            <h1>slovolingo</h1>
+          </div>
+
+        </div>
+
+        <div className="register-main">
+          <div className="sign-up">
+            <h1>Sign Up</h1>
+          </div>
+
+          <form action="/auth/register" method='POST' className="register-form">
+            <input type="text" name='userName' placeholder='User Name' className="register-space"
+              onChange={(e)=>this.handleInputChange(e)}/>
+            <input type="email" name='email' placeholder='Email' className="register-space"
+              onChange={(e)=>this.handleInputChange(e)}/>
+            <input type="password" name='password' placeholder='Password' className="register-space"
+              onChange={(e)=>this.handleInputChange(e)}/>
+            <input onClick={(e)=>this.props.handleRegisterSubmit(e, this.state)} type="submit" value='Create Profile' className="register-form-button"/>
+          </form>
+
+        </div>
+
+        <div className="register-footer">
+          <h2>Created By Dan Beebe</h2>
         </div>
 
       </div>
-
-      <div className="register-main">
-        <h1>Register Page</h1>
-      </div>
-
-      <div className="register-footer">
-        <h2>Created By Dan Beebe</h2>
-      </div>
-
-    </div>
-  )
+    )
+  }
 }
 
 export default Register;
