a1a265f3135cd98877f8045a4d7532bbfc1b0faa
Daniel Beebe <danwbeebe@gmail.com>
2017-09-12 09:42:49 -0400

    adjust css styles for welcome page

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -7,15 +7,37 @@ const Welcome = () => {
     <div className="welcome">
 
       <div className="welcome-header">
-       <h1>slavolingo</h1>
+
+        <div className="header-message">
+          <h1>slavolingo</h1>
+        </div>
+
+        <div className="login-button">
+          <h4>Login</h4>
+        </div>
+
       </div>
 
       <div className="welcome-main">
-        <h1>Learn the Slovene language for free. Forever.</h1>
+
+        <div className="welcome-left">
+          <h2>Image Here</h2>
+        </div>
+
+        <div className="welcome-right">
+          <div className="welcome-message">
+            <h1>Learn Slovene for free. Forever.</h1>
+          </div>
+
+          <div className="register-button">
+            <h3>Get Started</h3>
+          </div>
+        </div>
+
       </div>
 
       <div className="welcome-footer">
-       <h2>Created By Dan</h2>
+       <h2>Created By Dan Beebe</h2>
       </div>
 
     </div>
