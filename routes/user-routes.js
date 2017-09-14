d94c75f7132b82ba01c6755afa13d43fd17a8f61
Daniel Beebe <danwbeebe@gmail.com>
2017-09-14 12:00:14 -0400

    continue work on fixing error in registration of new user

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -3,5 +3,6 @@ const userRoutes = express.Router();
 const usersController = require('../controllers/users-controller');
 const authHelpers = require('../services/auth/auth-helpers');
 
+// Not sure if this file will be needed - consider deleting
 
 module.exports = userRoutes;
