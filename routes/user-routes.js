7861d442d011e3478d229f761d8fa94c1f9333d8
Daniel Beebe <danwbeebe@gmail.com>
2017-09-14 10:12:55 -0400

    add user routes file

---
+++ routes/user-routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const userRoutes = express.Router();
+const usersController = require('../controllers/users-controller');
+const authHelpers = require('../services/auth/auth-helpers');
+
+
+module.exports = userRoutes;
