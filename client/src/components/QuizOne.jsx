8487ebae86844ca4760781e1b364ccec534a35d6
Daniel Beebe <danwbeebe@gmail.com>
2017-09-15 14:06:12 -0400

    add css styles and layout to quiz one component

--- client/src/components/QuizOne.jsx
+++ client/src/components/QuizOne.jsx
@@ -1,29 +1,63 @@
-import React from 'react';
+import React, {Component} from 'react';
 
+class QuizOne extends Component {
+  constructor(){
+    super();
+      this.state = {
+        question: 1,
+        answer: 1,
+        wrong_one: 2,
+        wrong_two: 3,
+        done: false,
+      }
+  }
 
-const QuizOne = () => {
+  handleChangeQuestion() {
 
-  return (
-    <div className="quiz-one">
+  }
 
-      <div className="quiz-one-header">
+  handleRandomAnswerOrder() {
 
-        <div className="quiz-one-message">
-          <h1>slovolingo</h1>
+  }
+
+  render(){
+    return (
+      <div className="quiz-one">
+
+        <div className="quiz-one-header">
+
+          <div className="quiz-one-message">
+            <h1>slovolingo</h1>
+          </div>
+
+        </div>
+
+        <div className="quiz-one-main">
+
+          <div className="quiz-one-question">
+            {this.state.question}
+          </div>
+
+          <div className="quiz-one-choices">
+            <div className="quiz-one-answer-left">
+              {this.state.answer}
+            </div>
+            <div className="quiz-one-answer-center">
+              {this.state.wrong_one}
+            </div>
+            <div className="quiz-one-answer-right">
+              {this.state.wrong_two}
+            </div>
+          </div>
+        </div>
+
+        <div className="quiz-one-footer">
+          <h2>Created By Dan Beebe</h2>
         </div>
 
       </div>
-
-      <div className="quiz-one-main">
-        <h1>Quiz One Page</h1>
-      </div>
-
-      <div className="quiz-one-footer">
-        <h2>Created By Dan Beebe</h2>
-      </div>
-
-    </div>
-  )
+    )
+  }
 }
 
 export default QuizOne;
