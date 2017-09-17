51e979e478b50e39d75114ccf52bdd8c11d98a6a
Daniel Beebe <danwbeebe@gmail.com>
2017-09-17 16:36:43 -0400

    complete questions 1 through 5 for quiz 2

--- client/src/components/QuizTwo.jsx
+++ client/src/components/QuizTwo.jsx
@@ -119,157 +119,157 @@ class QuizTwo extends Component {
     audio.play();
     }
 
-// // question 2
-//     if ((this.state.number===1) && (randNum===0)) {
-//       this.setState({
-//         number: 2,
-//         question: "What does 'oni so' mean?",
-//         left: "They are",
-//         center: "I am",
-//         right: "He is",
-//         correct: "left"
-//       })
-//     var audio = new Audio(oniSo);
-//     audio.play();
-//     }
-//     if ((this.state.number===1) && (randNum===1)){
-//       this.setState({
-//         number: 2,
-//         question: "What does 'oni so' mean?",
-//         left: "He is",
-//         center: "They are",
-//         right: "I am",
-//         correct: "center"
-//       })
-//     var audio = new Audio(oniSo);
-//     audio.play();
-//     }
-//     if ((this.state.number===1) && (randNum===2)) {
-//       this.setState({
-//         number: 2,
-//         question: "What does 'oni so' mean?",
-//         left: "I am",
-//         center: "He is",
-//         right: "They are",
-//         correct: "right"
-//     })
-//     var audio = new Audio(oniSo);
-//     audio.play();
-//     }
+// question 2
+    if ((this.state.number===1) && (randNum===0)) {
+      this.setState({
+        number: 2,
+        question: "What does 'zivjo' mean?",
+        left: "Hi",
+        center: "Hello",
+        right: "Bye",
+        correct: "left"
+      })
+    var audio = new Audio(zivjo);
+    audio.play();
+    }
+    if ((this.state.number===1) && (randNum===1)){
+      this.setState({
+        number: 2,
+        question: "What does 'zivjo' mean?",
+        left: "Bye",
+        center: "Hi",
+        right: "Hello",
+        correct: "center"
+      })
+    var audio = new Audio(zivjo);
+    audio.play();
+    }
+    if ((this.state.number===1) && (randNum===2)) {
+      this.setState({
+        number: 2,
+        question: "What does 'zivjo' mean?",
+        left: "Hello",
+        center: "Bye",
+        right: "Hi",
+        correct: "right"
+    })
+    var audio = new Audio(zivjo);
+    audio.play();
+    }
 
-// // question 3
-//     if ((this.state.number===2) && (randNum===0)) {
-//       this.setState({
-//         number: 3,
-//         question: "What does 'vidva sta' mean?",
-//         left: "You two are",
-//         center: "You are",
-//         right: "She is",
-//         correct: "left"
-//       })
-//     var audio = new Audio(vidvaSta);
-//     audio.play();
-//     }
-//     if ((this.state.number===2) && (randNum===1)){
-//       this.setState({
-//         number: 3,
-//         question: "What does 'vidva sta' mean?",
-//         left: "She is",
-//         center: "You two are",
-//         right: "You are",
-//         correct: "center"
-//       })
-//     var audio = new Audio(vidvaSta);
-//     audio.play();
-//     }
-//     if ((this.state.number===2) && (randNum===2)) {
-//       this.setState({
-//         number: 3,
-//         question: "What does 'vidva sta' mean?",
-//         left: "You are",
-//         center: "She is",
-//         right: "You two are",
-//         correct: "right"
-//     })
-//     var audio = new Audio(vidvaSta);
-//     audio.play();
-//     }
+// question 3
+    if ((this.state.number===2) && (randNum===0)) {
+      this.setState({
+        number: 3,
+        question: "What does 'na svidenje' mean?",
+        left: "Good-bye",
+        center: "Bye",
+        right: "Hello",
+        correct: "left"
+      })
+    var audio = new Audio(naSvidenje);
+    audio.play();
+    }
+    if ((this.state.number===2) && (randNum===1)){
+      this.setState({
+        number: 3,
+        question: "What does 'na svidenje' mean?",
+        left: "Hello",
+        center: "Good-bye",
+        right: "Bye",
+        correct: "center"
+      })
+    var audio = new Audio(naSvidenje);
+    audio.play();
+    }
+    if ((this.state.number===2) && (randNum===2)) {
+      this.setState({
+        number: 3,
+        question: "What does 'na svidenje' mean?",
+        left: "Bye",
+        center: "Hello",
+        right: "Good-bye",
+        correct: "right"
+    })
+    var audio = new Audio(naSvidenje);
+    audio.play();
+    }
 
-// // question 4
-//     if ((this.state.number===3) && (randNum===0)) {
-//       this.setState({
-//         number: 4,
-//         question: "What does 'ona je' mean?",
-//         left: "She is",
-//         center: "He is",
-//         right: "They are",
-//         correct: "left"
-//       })
-//     var audio = new Audio(onaJe);
-//     audio.play();
-//     }
-//     if ((this.state.number===3) && (randNum===1)){
-//       this.setState({
-//         number: 4,
-//         question: "What does 'ona je' mean?",
-//         left: "They are",
-//         center: "She is",
-//         right: "He is",
-//         correct: "center"
-//       })
-//     var audio = new Audio(onaJe);
-//     audio.play();
-//     }
-//     if ((this.state.number===3) && (randNum===2)) {
-//       this.setState({
-//         number: 4,
-//         question: "What does 'ona je' mean?",
-//         left: "He is",
-//         center: "They are",
-//         right: "She is",
-//         correct: "right"
-//     })
-//     var audio = new Audio(onaJe);
-//     audio.play();
-//     }
+// question 4
+    if ((this.state.number===3) && (randNum===0)) {
+      this.setState({
+        number: 4,
+        question: "What does 'zdravo' mean?",
+        left: "Bye",
+        center: "Good-bye",
+        right: "Hi",
+        correct: "left"
+      })
+    var audio = new Audio(zdravo);
+    audio.play();
+    }
+    if ((this.state.number===3) && (randNum===1)){
+      this.setState({
+        number: 4,
+        question: "What does 'zdravo' mean?",
+        left: "Hi",
+        center: "Bye",
+        right: "Good-bye",
+        correct: "center"
+      })
+    var audio = new Audio(zdravo);
+    audio.play();
+    }
+    if ((this.state.number===3) && (randNum===2)) {
+      this.setState({
+        number: 4,
+        question: "What does 'zdravo' mean?",
+        left: "Good-bye",
+        center: "Hi",
+        right: "Bye",
+        correct: "right"
+    })
+    var audio = new Audio(zdravo);
+    audio.play();
+    }
 
-// // question 5
-//     if ((this.state.number===4) && (randNum===0)) {
-//       this.setState({
-//         number: 5,
-//         question: "What does 'mi smo' mean?",
-//         left: "We are",
-//         center: "I am",
-//         right: "They are",
-//         correct: "left"
-//       })
-//     var audio = new Audio(miSmo);
-//     audio.play();
-//     }
-//     if ((this.state.number===4) && (randNum===1)){
-//       this.setState({
-//         number: 5,
-//         question: "What does 'mi smo' mean?",
-//         left: "They are",
-//         center: "We are",
-//         right: "I am",
-//         correct: "center"
-//       })
-//     var audio = new Audio(miSmo);
-//     audio.play();
-//     }
-//     if ((this.state.number===4) && (randNum===2)) {
-//       this.setState({
-//         number: 5,
-//         question: "What does 'mi smo' mean?",
-//         left: "I am",
-//         center: "They are",
-//         right: "We are",
-//         correct: "right"
-//     })
-//     var audio = new Audio(miSmo);
-//     audio.play();
-//     }
+// question 5
+    if ((this.state.number===4) && (randNum===0)) {
+      this.setState({
+        number: 5,
+        question: "What does 'adijo' mean?",
+        left: "Bye",
+        center: "Good-bye",
+        right: "Hi",
+        correct: "left"
+      })
+    var audio = new Audio(adijo);
+    audio.play();
+    }
+    if ((this.state.number===4) && (randNum===1)){
+      this.setState({
+        number: 5,
+        question: "What does 'adijo' mean?",
+        left: "Hi",
+        center: "Bye",
+        right: "Good-bye",
+        correct: "center"
+      })
+    var audio = new Audio(adijo);
+    audio.play();
+    }
+    if ((this.state.number===4) && (randNum===2)) {
+      this.setState({
+        number: 5,
+        question: "What does 'adijo' mean?",
+        left: "Good-bye",
+        center: "Hi",
+        right: "Bye",
+        correct: "right"
+    })
+    var audio = new Audio(adijo);
+    audio.play();
+    }
 
 // // question 6
 //     if ((this.state.number===5) && (randNum===0)) {
