ba4076cdbd71bb86d429027c14f0f9b39a22fcd3
Daniel Beebe <danwbeebe@gmail.com>
2017-09-17 18:26:45 -0400

    complete quiz 3 content

--- client/src/components/QuizThree.jsx
+++ client/src/components/QuizThree.jsx
@@ -27,6 +27,333 @@ class QuizThree extends Component {
         clicked: " ",
         done: false,
       }
+    this.randomOrder = this.randomOrder.bind(this);
+    this.clickLeft = this.clickLeft.bind(this);
+    this.clickCenter = this.clickCenter.bind(this);
+    this.clickRight = this.clickRight.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.checkCorrect = this.checkCorrect.bind(this);
+  }
+
+  componentDidMount() {
+    this.randomOrder();
+  }
+
+  clickLeft() {
+    if (this.state.correct==="left") {
+      this.checkCorrect();
+    }
+    else {
+      this.isWrong();
+    }
+  }
+
+  clickCenter() {
+    if (this.state.correct==="center") {
+      this.checkCorrect();
+    }
+    else {
+      this.isWrong();
+    }
+  }
+
+  clickRight() {
+    if (this.state.correct==="right") {
+      this.checkCorrect();
+    }
+    else {
+      this.isWrong();
+    }
+  }
+
+  checkCorrect() {
+    var audio = new Audio(ringSound);
+    audio.play();
+    this.componentDidMount();
+  }
+
+  isWrong() {
+    var audio = new Audio(knockSound);
+    audio.play();
+  }
+
+  randomOrder() {
+    var randNum = Math.floor(Math.random() * 3);
+
+// question 1
+    if ((this.state.number===0) && (randNum===0)) {
+      this.setState({
+        number: 1,
+        question: "What does 'ponedeljek' mean?",
+        left: "Monday",
+        center: "Tuesday",
+        right: "Wednesday",
+        correct: "left"
+      })
+    var audio = new Audio(ponedeljek);
+    audio.play();
+    }
+    if ((this.state.number===0) && (randNum===1)){
+      this.setState({
+        number: 1,
+        question: "What does 'ponedeljek' mean?",
+        left: "Wednesday",
+        center: "Monday",
+        right: "Tuesday",
+        correct: "center"
+      })
+    var audio = new Audio(ponedeljek);
+    audio.play();
+    }
+    if ((this.state.number===0) && (randNum===2)) {
+      this.setState({
+        number: 1,
+        question: "What does 'ponedeljek' mean?",
+        left: "Tuesday",
+        center: "Wednesday",
+        right: "Monday",
+        correct: "right"
+      })
+    var audio = new Audio(ponedeljek);
+    audio.play();
+    }
+
+// question 2
+    if ((this.state.number===1) && (randNum===0)) {
+      this.setState({
+        number: 2,
+        question: "What does 'petek' mean?",
+        left: "Friday",
+        center: "Monday",
+        right: "Wednesday",
+        correct: "left"
+      })
+    var audio = new Audio(petek);
+    audio.play();
+    }
+    if ((this.state.number===1) && (randNum===1)){
+      this.setState({
+        number: 2,
+        question: "What does 'petek' mean?",
+        left: "Wednesday",
+        center: "Friday",
+        right: "Monday",
+        correct: "center"
+      })
+    var audio = new Audio(petek);
+    audio.play();
+    }
+    if ((this.state.number===1) && (randNum===2)) {
+      this.setState({
+        number: 2,
+        question: "What does 'petek' mean?",
+        left: "Monday",
+        center: "Wednesday",
+        right: "Friday",
+        correct: "right"
+    })
+    var audio = new Audio(petek);
+    audio.play();
+    }
+
+// question 3
+    if ((this.state.number===2) && (randNum===0)) {
+      this.setState({
+        number: 3,
+        question: "What does 'sobota' mean?",
+        left: "Saturday",
+        center: "Friday",
+        right: "Sunday",
+        correct: "left"
+      })
+    var audio = new Audio(sobota);
+    audio.play();
+    }
+    if ((this.state.number===2) && (randNum===1)){
+      this.setState({
+        number: 3,
+        question: "What does 'sobota' mean?",
+        left: "Sunday",
+        center: "Saturday",
+        right: "Friday",
+        correct: "center"
+      })
+    var audio = new Audio(sobota);
+    audio.play();
+    }
+    if ((this.state.number===2) && (randNum===2)) {
+      this.setState({
+        number: 3,
+        question: "What does 'sobota' mean?",
+        left: "Friday",
+        center: "Sunday",
+        right: "Saturday",
+        correct: "right"
+    })
+    var audio = new Audio(sobota);
+    audio.play();
+    }
+
+// question 4
+    if ((this.state.number===3) && (randNum===0)) {
+      this.setState({
+        number: 4,
+        question: "What does 'torek' mean?",
+        left: "Tuesday",
+        center: "Wednesday",
+        right: "Thursday",
+        correct: "left"
+      })
+    var audio = new Audio(torek);
+    audio.play();
+    }
+    if ((this.state.number===3) && (randNum===1)){
+      this.setState({
+        number: 4,
+        question: "What does 'torek' mean?",
+        left: "Thursday",
+        center: "Tuesday",
+        right: "Wednesday",
+        correct: "center"
+      })
+    var audio = new Audio(torek);
+    audio.play();
+    }
+    if ((this.state.number===3) && (randNum===2)) {
+      this.setState({
+        number: 4,
+        question: "What does 'torek' mean?",
+        left: "Wednesday",
+        center: "Thursday",
+        right: "Tuesday",
+        correct: "right"
+    })
+    var audio = new Audio(torek);
+    audio.play();
+    }
+
+// question 5
+    if ((this.state.number===4) && (randNum===0)) {
+      this.setState({
+        number: 5,
+        question: "What does 'četrtek' mean?",
+        left: "Thursday",
+        center: "Monday",
+        right: "Tuesday",
+        correct: "left"
+      })
+    var audio = new Audio(cetrtek);
+    audio.play();
+    }
+    if ((this.state.number===4) && (randNum===1)){
+      this.setState({
+        number: 5,
+        question: "What does 'četrtek' mean?",
+        left: "Tuesday",
+        center: "Thursday",
+        right: "Monday",
+        correct: "center"
+      })
+    var audio = new Audio(cetrtek);
+    audio.play();
+    }
+    if ((this.state.number===4) && (randNum===2)) {
+      this.setState({
+        number: 5,
+        question: "What does 'četrtek' mean?",
+        left: "Monday",
+        center: "Tuesday",
+        right: "Thursday",
+        correct: "right"
+    })
+    var audio = new Audio(cetrtek);
+    audio.play();
+    }
+
+// question 6
+    if ((this.state.number===5) && (randNum===0)) {
+      this.setState({
+        number: 6,
+        question: "What does 'sreda' mean?",
+        left: "Wednesday",
+        center: "Thursday",
+        right: "Friday",
+        correct: "left"
+      })
+    var audio = new Audio(sreda);
+    audio.play();
+    }
+    if ((this.state.number===5) && (randNum===1)){
+      this.setState({
+        number: 6,
+        question: "What does 'sreda' mean?",
+        left: "Friday",
+        center: "Wednesday",
+        right: "Thursday",
+        correct: "center"
+      })
+    var audio = new Audio(sreda);
+    audio.play();
+    }
+    if ((this.state.number===5) && (randNum===2)) {
+      this.setState({
+        number: 6,
+        question: "What does 'sreda' mean?",
+        left: "Thursday",
+        center: "Friday",
+        right: "Wednesday",
+        correct: "right"
+    })
+    var audio = new Audio(sreda);
+    audio.play();
+    }
+
+// question 7
+    if ((this.state.number===6) && (randNum===0)) {
+      this.setState({
+        number: 7,
+        question: "What does 'nedelja' mean?",
+        left: "Sunday",
+        center: "Monday",
+        right: "Saturday",
+        correct: "left"
+      })
+    var audio = new Audio(nedelja);
+    audio.play();
+    }
+    if ((this.state.number===6) && (randNum===1)){
+      this.setState({
+        number: 7,
+        question: "What does 'nedelja' mean?",
+        left: "Saturday",
+        center: "Sunday",
+        right: "Monday",
+        correct: "center"
+      })
+    var audio = new Audio(nedelja);
+    audio.play();
+    }
+    if ((this.state.number===6) && (randNum===2)) {
+      this.setState({
+        number: 7,
+        question: "What does 'nedelja' mean?",
+        left: "Monday",
+        center: "Saturday",
+        right: "Sunday",
+        correct: "right"
+    })
+    var audio = new Audio(nedelja);
+    audio.play();
+    }
+
+    if (this.state.number===7) {
+    this.setState({
+      question: "Congratulations! You Finished Quiz Three!",
+      left: " ",
+      center: " ",
+      right: "  ",
+      correct: "  "
+    })}
   }
 
   render(){
