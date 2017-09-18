08d93740a6716e759826969dc3c90ddffa0f07ab
Daniel Beebe <danwbeebe@gmail.com>
2017-09-18 10:17:23 -0400

    add method in word component for random word to appear on main page

--- client/src/components/Word.jsx
+++ client/src/components/Word.jsx
@@ -1,12 +1,32 @@
 import React, {Component} from 'react';
 
 class Word extends Component {
+  constructor(){
+    super();
+      this.state = {
+        word: " ",
+      }
+    }
+
+  componentDidMount() {
+    this.randomOrder();
+  }
+
+  randomOrder() {
+    var randNum = Math.floor(Math.random() * 1);
+
+    if (randNum===0) {
+      this.setState({
+        word: "'naslov' means address",
+      })
+    }
+  }
 
   render(){
     return (
-      <div>
-        <h2>Random Word to Learn</h2>
-        <h4>"Word" means: translation</h4>
+      <div className="random-word">
+        <h3 className="random-word-intro">Random Word to Learn:</h3>
+        <h4>{this.state.word}</h4>
       </div>
     )
   }
